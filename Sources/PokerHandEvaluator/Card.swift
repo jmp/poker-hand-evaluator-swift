0e34b28b23b8dfac7d3c86d8c7d7c896d5832a7c
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-16 22:47:08 +0200

    Add card value

--- Sources/PokerHandEvaluator/Card.swift
+++ Sources/PokerHandEvaluator/Card.swift
@@ -1,9 +1,17 @@
 public struct Card: Equatable, Hashable {
     public let rank: Rank
     public let suit: Suit
+    public let value: Int
     
     public init(_ rank: Rank, _ suit: Suit) {
         self.rank = rank
         self.suit = suit
+        self.value = calculateValue(rank, suit)
     }
+}
+
+private let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+
+private func calculateValue(_ rank: Rank, _ suit: Suit) -> Int {
+    return (1 << (rank.rawValue + 16)) | suit.rawValue | (rank.rawValue << 8) | primes[rank.rawValue]
 }
--- Tests/PokerHandEvaluatorTests/CardTests.swift
+++ Tests/PokerHandEvaluatorTests/CardTests.swift
@@ -11,9 +11,15 @@ final class CardTests: XCTestCase {
         let card = Card(.king, .hearts)
         XCTAssertEqual(.hearts, card.suit)
     }
+    
+    func testValue() {
+        let card = Card(.five, .spades)
+        XCTAssertEqual(0b00000000000010000001001100000111, card.value)
+    }
 
     static var allTests = [
         ("testRank", testRank),
         ("testSuit", testSuit),
+        ("testValue", testValue),
     ]
 }
