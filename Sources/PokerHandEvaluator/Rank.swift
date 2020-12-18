1a15f21864a208ee2cf5b0a0f0853344e3ea432e
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-19 01:31:35 +0200

    Simplify ranks

--- Sources/PokerHandEvaluator/Rank.swift
+++ Sources/PokerHandEvaluator/Rank.swift
@@ -1,15 +1,15 @@
 public enum Rank: Int {
-    case deuce = 0
-    case trey = 1
-    case four = 2
-    case five = 3
-    case six = 4
-    case seven = 5
-    case eight = 6
-    case nine = 7
-    case ten = 8
-    case jack = 9
-    case queen = 10
-    case king = 11
-    case ace = 12
+    case deuce
+    case trey
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case ten
+    case jack
+    case queen
+    case king
+    case ace
 }
