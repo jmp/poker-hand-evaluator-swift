65cae6e448fa0c95ed9ab08b9b6b1848ede4d1e3
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-19 12:17:00 +0200

    Simplify evaluation

--- Sources/PokerHandEvaluator/Hand.swift
+++ Sources/PokerHandEvaluator/Hand.swift
@@ -11,22 +11,15 @@ public struct Hand {
     }
     
     public var value: Int {
-        let c1 = cards[0].value
-        let c2 = cards[1].value
-        let c3 = cards[2].value
-        let c4 = cards[3].value
-        let c5 = cards[4].value
-        let index = (c1 | c2 | c3 | c4 | c5) >> 16
-        
-        if c1 & c2 & c3 & c4 & c5 & 0xF000 != 0 {
+        let cardValues = cards.map(\.value)
+        let index = cardValues.reduce(0, |) >> 16
+        if cardValues.reduce(0xF000, &) != 0 {
             return flushes[index]
         }
-
         if uniques[index] != 0 {
             return uniques[index]
         }
-        
-        let product = (c1 & 0xFF) * (c2 & 0xFF) * (c3 & 0xFF) * (c4 & 0xFF) * (c5 & 0xFF)
+        let product = cardValues.map { $0 & 0xFF }.reduce(1, *)
         let tmp = hash(product)
         return values[tmp]
     }
