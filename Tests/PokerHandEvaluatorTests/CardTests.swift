ee9a9963ceb5eb25f566fc7139ff558654f44ab9
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-19 16:48:59 +0200

    Add more complete card value tests

--- Tests/PokerHandEvaluatorTests/CardTests.swift
+++ Tests/PokerHandEvaluatorTests/CardTests.swift
@@ -25,8 +25,61 @@ final class CardTests: XCTestCase {
     }
     
     func testValue() {
-        let card = Card(.five, .spades)
-        XCTAssertEqual(0b00000000000010000001001100000111, card.value)
+        XCTAssertEqual(0b00000000000000011000000000000010, Card(.deuce, .clubs).value)
+        XCTAssertEqual(0b00000000000000101000000100000011, Card(.trey, .clubs).value)
+        XCTAssertEqual(0b00000000000001001000001000000101, Card(.four, .clubs).value)
+        XCTAssertEqual(0b00000000000010001000001100000111, Card(.five, .clubs).value)
+        XCTAssertEqual(0b00000000000100001000010000001011, Card(.six, .clubs).value)
+        XCTAssertEqual(0b00000000001000001000010100001101, Card(.seven, .clubs).value)
+        XCTAssertEqual(0b00000000010000001000011000010001, Card(.eight, .clubs).value)
+        XCTAssertEqual(0b00000000100000001000011100010011, Card(.nine, .clubs).value)
+        XCTAssertEqual(0b00000001000000001000100000010111, Card(.ten, .clubs).value)
+        XCTAssertEqual(0b00000010000000001000100100011101, Card(.jack, .clubs).value)
+        XCTAssertEqual(0b00000100000000001000101000011111, Card(.queen, .clubs).value)
+        XCTAssertEqual(0b00001000000000001000101100100101, Card(.king, .clubs).value)
+        XCTAssertEqual(0b00010000000000001000110000101001, Card(.ace, .clubs).value)
+
+        XCTAssertEqual(0b00000000000000010100000000000010, Card(.deuce, .diamonds).value)
+        XCTAssertEqual(0b00000000000000100100000100000011, Card(.trey, .diamonds).value)
+        XCTAssertEqual(0b00000000000001000100001000000101, Card(.four, .diamonds).value)
+        XCTAssertEqual(0b00000000000010000100001100000111, Card(.five, .diamonds).value)
+        XCTAssertEqual(0b00000000000100000100010000001011, Card(.six, .diamonds).value)
+        XCTAssertEqual(0b00000000001000000100010100001101, Card(.seven, .diamonds).value)
+        XCTAssertEqual(0b00000000010000000100011000010001, Card(.eight, .diamonds).value)
+        XCTAssertEqual(0b00000000100000000100011100010011, Card(.nine, .diamonds).value)
+        XCTAssertEqual(0b00000001000000000100100000010111, Card(.ten, .diamonds).value)
+        XCTAssertEqual(0b00000010000000000100100100011101, Card(.jack, .diamonds).value)
+        XCTAssertEqual(0b00000100000000000100101000011111, Card(.queen, .diamonds).value)
+        XCTAssertEqual(0b00001000000000000100101100100101, Card(.king, .diamonds).value)
+        XCTAssertEqual(0b00010000000000000100110000101001, Card(.ace, .diamonds).value)
+
+        XCTAssertEqual(0b00000000000000010010000000000010, Card(.deuce, .hearts).value)
+        XCTAssertEqual(0b00000000000000100010000100000011, Card(.trey, .hearts).value)
+        XCTAssertEqual(0b00000000000001000010001000000101, Card(.four, .hearts).value)
+        XCTAssertEqual(0b00000000000010000010001100000111, Card(.five, .hearts).value)
+        XCTAssertEqual(0b00000000000100000010010000001011, Card(.six, .hearts).value)
+        XCTAssertEqual(0b00000000001000000010010100001101, Card(.seven, .hearts).value)
+        XCTAssertEqual(0b00000000010000000010011000010001, Card(.eight, .hearts).value)
+        XCTAssertEqual(0b00000000100000000010011100010011, Card(.nine, .hearts).value)
+        XCTAssertEqual(0b00000001000000000010100000010111, Card(.ten, .hearts).value)
+        XCTAssertEqual(0b00000010000000000010100100011101, Card(.jack, .hearts).value)
+        XCTAssertEqual(0b00000100000000000010101000011111, Card(.queen, .hearts).value)
+        XCTAssertEqual(0b00001000000000000010101100100101, Card(.king, .hearts).value)
+        XCTAssertEqual(0b00010000000000000010110000101001, Card(.ace, .hearts).value)
+
+        XCTAssertEqual(0b00000000000000010001000000000010, Card(.deuce, .spades).value)
+        XCTAssertEqual(0b00000000000000100001000100000011, Card(.trey, .spades).value)
+        XCTAssertEqual(0b00000000000001000001001000000101, Card(.four, .spades).value)
+        XCTAssertEqual(0b00000000000010000001001100000111, Card(.five, .spades).value)
+        XCTAssertEqual(0b00000000000100000001010000001011, Card(.six, .spades).value)
+        XCTAssertEqual(0b00000000001000000001010100001101, Card(.seven, .spades).value)
+        XCTAssertEqual(0b00000000010000000001011000010001, Card(.eight, .spades).value)
+        XCTAssertEqual(0b00000000100000000001011100010011, Card(.nine, .spades).value)
+        XCTAssertEqual(0b00000001000000000001100000010111, Card(.ten, .spades).value)
+        XCTAssertEqual(0b00000010000000000001100100011101, Card(.jack, .spades).value)
+        XCTAssertEqual(0b00000100000000000001101000011111, Card(.queen, .spades).value)
+        XCTAssertEqual(0b00001000000000000001101100100101, Card(.king, .spades).value)
+        XCTAssertEqual(0b00010000000000000001110000101001, Card(.ace, .spades).value)
     }
     
     func testDescription() {
@@ -95,4 +148,23 @@ final class CardTests: XCTestCase {
         ("testValue", testValue),
         ("testDescription", testDescription),
     ]
+}
+
+extension Card {
+    var binaryDescription: String {
+        var binaryString = ""
+        var internalNumber = value
+        var counter = 0
+
+        for _ in (1...32) {
+            binaryString.insert(contentsOf: "\(internalNumber & 1)", at: binaryString.startIndex)
+            internalNumber >>= 1
+            counter += 1
+            if counter % 4 == 0 {
+                binaryString.insert(contentsOf: "", at: binaryString.startIndex)
+            }
+        }
+
+        return "0b" + binaryString
+    }
 }
