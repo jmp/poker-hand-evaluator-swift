8c2c623c461acd2206f93a63f03ba24458e73462
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2020-12-19 13:04:59 +0200

    Add suit description

--- Sources/PokerHandEvaluator/Suit.swift
+++ Sources/PokerHandEvaluator/Suit.swift
@@ -1,6 +1,15 @@
-public enum Suit: Int {
+public enum Suit: Int, CustomStringConvertible {
     case clubs = 0x8000
     case diamonds = 0x4000
     case hearts = 0x2000
     case spades = 0x1000
+    
+    public var description: String {
+        switch self {
+        case .clubs: return "♣"
+        case .diamonds: return "♦"
+        case .hearts: return "❤"
+        case .spades: return "♠"
+        }
+    }
 }
---
+++ Tests/PokerHandEvaluatorTests/SuitTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+import PokerHandEvaluator
+
+final class SuitTests: XCTestCase {
+    func testDescription() {
+        XCTAssertEqual("♠", Suit.spades.description)
+        XCTAssertEqual("❤", Suit.hearts.description)
+        XCTAssertEqual("♦", Suit.diamonds.description)
+        XCTAssertEqual("♣", Suit.clubs.description)
+    }
+
+    static var allTests = [
+        ("testDescription", testDescription),
+    ]
+}
